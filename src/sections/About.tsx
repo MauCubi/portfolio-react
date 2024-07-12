a0b4949c3a9da36a91e5dee0950cc6f5711ac2d2
Mauro <dantuchiii@gmail.com>
2024-07-12 16:08:57 -0300

    path fix

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,7 +1,7 @@
 
 const About = () => {
   return (
-    <div className="h-[100vh] flex justify-center bg-[url('/src/assets/bg.jpg')] bg-cover">
+    <div className="h-[100vh] flex justify-center bg-[url('/assets/bg.jpg')] bg-cover">
 
       <div className='flex gap-10'>
 
@@ -22,17 +22,17 @@ const About = () => {
           <div className='flex flex-row justify-center mt-4 gap-6'>
 
             <button className='relative flex overflow-hidden flex-row align-middle items-center justify-center rounded-full border border-slate-400 py-1.5 px-3 text-sm bg-slate-100 shadow-2xl transition-all before:absolute before:h-0 before:w-0 before:rounded-full before:bg-slate-300 before:duration-500 before:ease-out hover:shadow-slate-300 hover:before:h-56 hover:before:w-56'>
-              <img src='../src/assets/icons/icons8-github.svg' className='w-5 mr-1 z-10'/>
+              <img src='/assets/icons/icons8-github.svg' className='w-5 mr-1 z-10'/>
               <span className='z-10 text-gray-800'>GitHub</span>
             </button>
 
             <button className='relative flex overflow-hidden flex-row align-middle items-center justify-center rounded-full border border-slate-400 py-1.5 px-3 text-sm bg-slate-100 shadow-2xl transition-all before:absolute before:h-0 before:w-0 before:rounded-full before:bg-slate-300 before:duration-500 before:ease-out hover:shadow-slate-300 hover:before:h-56 hover:before:w-56'>
-              <img src='/src/assets/icons/107178_circle_linkedin_icon.svg' className='w-5 mr-1 z-10'/>
+              <img src='/assets/icons/107178_circle_linkedin_icon.svg' className='w-5 mr-1 z-10'/>
               <span className='z-10 text-gray-800'>Linkedin</span>
             </button>
 
             <button className='relative flex overflow-hidden flex-row align-middle items-center justify-center rounded-full border border-slate-400 py-1.5 px-3 text-sm bg-slate-100 shadow-2xl transition-all before:absolute before:h-0 before:w-0 before:rounded-full before:bg-slate-300 before:duration-500 before:ease-out hover:shadow-slate-300 hover:before:h-56 hover:before:w-56'>
-              <img src='/src/assets/icons/icons8-resume-30.png' className='w-5 mr-1 z-10'/>
+              <img src='/assets/icons/icons8-resume-30.png' className='w-5 mr-1 z-10'/>
               <span className='z-10 text-gray-800'>Curriculum</span>
             </button>
           </div>
--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -10,32 +10,32 @@ const Skills = () => {
       <div className='flex mt-6 justify-center gap-12'>
 
         <div className='flex flex-col align-middle justify-center items-center'>
-          <img src="/src/assets/skills/HTML5.svg" alt="HTML" className='w-[50px]' />
+          <img src="/assets/skills/HTML5.svg" alt="HTML" className='w-[50px]' />
           <p className='skills-name'>HTML</p>
         </div>
 
         <div className='flex flex-col align-middle justify-center items-center'>
-          <img src="/src/assets/skills/CSS3.svg" alt="CSS" className='w-[50px]' />
+          <img src="/assets/skills/CSS3.svg" alt="CSS" className='w-[50px]' />
           <p className='skills-name'>CSS</p>
         </div>
 
         <div className='flex flex-col align-middle justify-center items-center'>
-          <img src="/src/assets/skills/JavaScript.svg" alt="JavaScript" className='w-[50px]' />
+          <img src="/assets/skills/JavaScript.svg" alt="JavaScript" className='w-[50px]' />
           <p className='skills-name'>JavaScript</p>
         </div>
 
         <div className='flex flex-col align-middle justify-center items-center'>
-          <img src="/src/assets/skills/TypeScript.svg" alt="JavaScript" className='w-[50px]' />
+          <img src="/assets/skills/TypeScript.svg" alt="JavaScript" className='w-[50px]' />
           <p className='skills-name'>TypeScript</p>
         </div>
 
         <div className='flex flex-col align-middle justify-center items-center'>
-          <img src="/src/assets/skills/React.svg" alt="JavaScript" className='w-[50px]' />
+          <img src="/assets/skills/React.svg" alt="JavaScript" className='w-[50px]' />
           <p className='skills-name'>React.js</p>
         </div>
 
         <div className='flex flex-col align-middle justify-center items-center'>
-          <img src="/src/assets/skills/Node.js.svg" alt="JavaScript" className='w-[50px]' />
+          <img src="/assets/skills/Node.js.svg" alt="JavaScript" className='w-[50px]' />
           <p className='skills-name'>Node.js</p>
         </div>
 

cc358c2b15a0eba5861fbd19f4a1c47536e2aadc
Mauro <dantuchiii@gmail.com>
2024-07-30 19:21:59 -0300

    More projects added

--- src/db/projects.ts
+++ src/db/projects.ts
@@ -5,18 +5,45 @@ export const projects = [
         title: "Red Social de Recetas",
         img: '/assets/projects/rece.png',
         description: "Descripcion de todo el proyecto, asi tipo re loco viste tiene que ser bastantelargo y bien descriptivo, asi parece todo re cheto y sin dejar espacios en blanco, aca ya no se que mas escribir",
-        technologies: ["React", "TypeScript","HTML5","CSS3","Tailwind CSS","MongoDB"],
+        technologies: ["React", "TypeScript","HTML5","CSS3","Material UI","MongoDB"],
         github: "https://github.com/MauCubi/react-recipe-fullstack",
         demo: "https://cubirecetitas.netlify.app/"
     },
     {
         id: 2,
-        title: "Projecto numero 2",
-        img: '/assets/projects/rece1.png',
+        title: "App de Clima",
+        img: '/assets/projects/climapp.png',
         description: "Descripcion de todo el proyecto, asi tipo re loco viste tiene que ser bastantelargo y bien descriptivo, asi parece todo re cheto y sin dejar espacios en blanco, aca ya no se que mas escribir",
-        technologies: ["React", "TypeScript","HTML5","CSS3","Tailwind CSS","MongoDB"],
+        technologies: ["React", "JavaScript","HTML5","CSS3","Material UI"],
+        github: "https://github.com/MauCubi/clima-react-app",
+        demo: "https://climapp-macubi.netlify.app/"
+    },
+    {
+        id: 3,
+        title: "Color Todo App",
+        img: '/assets/projects/todoapp.png',
+        description: "Descripcion de todo el proyecto, asi tipo re loco viste tiene que ser bastantelargo y bien descriptivo, asi parece todo re cheto y sin dejar espacios en blanco, aca ya no se que mas escribir",
+        technologies: ["React", "TypeScript","HTML5","CSS3","Material UI","MongoDB"],
+        github: "https://github.com/MauCubi/todolist-react-app",
+        demo: "https://todolist-macubi.netlify.app/"
+    },
+    {
+        id: 4,
+        title: "AluGeek",
+        img: '/assets/projects/Alugeek.png',
+        description: "Descripcion de todo el proyecto, asi tipo re loco viste tiene que ser bastantelargo y bien descriptivo, asi parece todo re cheto y sin dejar espacios en blanco, aca ya no se que mas escribir",
+        technologies: ["Javascript","HTML5","CSS3"],
         github: "#",
         demo: "#"
+    },
+    {
+        id: 5,
+        title: "Encriptador de Texto",
+        img: '/assets/projects/encrypt.png',
+        description: "Descripcion de todo el proyecto, asi tipo re loco viste tiene que ser bastantelargo y bien descriptivo, asi parece todo re cheto y sin dejar espacios en blanco, aca ya no se que mas escribir",
+        technologies: ["React", "TypeScript","HTML5","CSS3","Tailwind CSS"],
+        github: "https://github.com/MauCubi/react-text-encrypter",
+        demo: "https://text-encrypter.netlify.app/"
     },
 
 ];
